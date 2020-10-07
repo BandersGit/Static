61f12fbe3613c211c779c28463c127475c6424f5
BandersGit <andy.i.flaskan@gmail.com>
2020-10-07 10:19:43 +0200

    static

--- Static/Program.cs
+++ Static/Program.cs
@@ -6,6 +6,14 @@ namespace Static
     {
         static void Main(string[] args)
         {
+
+            // Static kommer att göra så att variabeln / metoden tillhör klassen och inte instansen
+
+            Enemy e1 = new Enemy();
+            Enemy e2 = new Enemy();
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
